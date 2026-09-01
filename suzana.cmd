4796cd0704673f491ba4309442551414920f4a50
dogneo31-ctrl <dogneo31@gmail.com>
2026-09-01 15:53:18 -0300

    first commit

--- suzana.cmd
+++ suzana.cmd
@@ -5,3 +5,4 @@ git commit -m "first commit"
 git branch -M main
 ::git remote add origin https://github.com/suzana-art/aula4.git
 git push -u origin main
+start "" https://github.com/suzana-art/aula4.git
