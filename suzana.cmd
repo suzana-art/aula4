3f79e0c2f873557c18f3b036773922435c490322
dogneo31-ctrl <dogneo31@gmail.com>
2026-09-01 15:49:46 -0300

    first commit

---
+++ suzana.cmd
@@ -0,0 +1,7 @@
+echo "# aula4" > README.md
+git init
+git add . -v
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/suzana-art/aula4.git
+git push -u origin main
